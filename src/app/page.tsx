7c81cfdfcf1e96277affb07e5bab05cf11da4637
vikasgodara290 <vikasgodara290@gmail.com>
2025-06-23 23:35:24 +0530

    landing page skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      hi there 
+    <div className="bg-primary-bg-color w-screen h-screen flex justify-center items-center">
+      {/* main div */}
+      <div className="w-[96%] h-11/12 rounded-[10px] bg-secondary-bg-color opacity-50 border-1
+      border-primary-border-color">
+        {/* nav bar */}
+        <div className="border-b-1 border-primary-border-color h-[50px]">
+
+        </div>
+      </div>
     </div>
   );
 }
